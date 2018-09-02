21c497594a1215ff997d0ad465eca4469197957d
MarGio <tenoriolopezm@gmail.com>
2018-09-01 22:34:08 -0500

    Unidad1

---
+++ Unidad 1 Movil/LocalBRTester-master/app/src/androidTest/java/testerapp/localbroadcast/codetutor/youtube/edu/localbrtester/ApplicationTest.java
@@ -0,0 +1,13 @@
+package testerapp.localbroadcast.codetutor.youtube.edu.localbrtester;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
